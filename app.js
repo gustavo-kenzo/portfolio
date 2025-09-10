b1d3c6d2e5a8a15a3e215395a560d024a55f9d0f
Gustavo <sgkenzo212007@gmail.com>
2025-09-10 00:29:49 -0300

    Fixed issues: logos not appearing (contact.css), inconsistent paths (projects.html), font-weight syntax error (contact.css), changing all  images to web path

--- app.js
+++ app.js
@@ -15,31 +15,3 @@ function clickBurguer() {
         icon.style.transform = 'scaleY(1) scaleX(1.5)';
     }
 }
-
-// Fechar menu ao clicar em um link
-// document.querySelectorAll('.cabecalho__menu-burguer__menu__link').forEach(link => {
-//     link.addEventListener('click', () => {
-//         const menu = document.getElementById('menu');
-//         const icon = document.querySelector('.material-icons');
-
-//         if (menu.classList.contains('expandido')) {
-//             menu.classList.remove('expandido');
-//             icon.style.transform = 'scale(1)';
-//             // icon.textContent = 'menu';
-//         }
-//     });
-// });
-
-// Fechar menu ao pressionar ESC
-// document.addEventListener('keydown', (e) => {
-//     if (e.key === 'Escape') {
-//         const menu = document.getElementById('menu');
-//         const icon = document.querySelector('.material-icons');
-
-//         if (menu.classList.contains('expandido')) {
-//             menu.classList.remove('expandido');
-//             icon.style.transform = 'scale(1)';
-//             // icon.textContent = 'menu';
-//         }
-//     }
-// });
